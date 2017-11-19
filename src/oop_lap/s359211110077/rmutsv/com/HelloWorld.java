1e4e01e48cd4407f3734811d69c99b7fe2a0bc3b
xziz <aziz_0822@hotmail.com>
2017-11-19 14:50:56 +0700

    Testing upload to github

---
+++ src/oop_lap/s359211110077/rmutsv/com/HelloWorld.java
@@ -0,0 +1,14 @@
+package oop_lap.s359211110077.rmutsv.com;
+
+public class HelloWorld {
+
+//    create main method
+public static void main(String[] args) {
+
+    System.out.println("Hello world,I'am aziz");
+
+
+
+
+}//main
+}//class

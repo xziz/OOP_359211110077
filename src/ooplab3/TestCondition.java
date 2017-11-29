0cceddfec881bbbbca798c84c1bb27b957f2a7f8
LAB_COM-MT_1 <aziz_0822@hotmail.com>
2017-11-29 09:36:43 +0700

    Test codition

---
+++ src/ooplab3/TestCondition.java
@@ -0,0 +1,27 @@
+package ooplab3;
+
+public class TestCondition {
+
+    public static void main(String[] args) {
+//      if
+        int x = 10, y = 20;
+        if (x > y)
+            System.out.print("x less than y");
+        System.out.print("OOP");
+
+//            if-else
+        x = 30;
+        if (x < y)
+            System.out.print("x less than y");
+        else
+            System.out.print("x more than y");
+//        if-else-if
+        if (x < 30) System.out.print("x less than 30");
+        else if (x > 30) System.out.print("x more than 30");
+        else System.out.print("x equal 30");
+
+
+    } //main
+
+
+} //class

18270f199a4cc0b19df79c2703313172415eb81d
xziz <aziz_0822@hotmail.com>
2017-12-06 10:45:00 +0700

    Test NestedLoop

---
+++ src/ooplab3/TestNestedLoop.java
@@ -0,0 +1,23 @@
+package ooplab3;
+
+public class TestNestedLoop {
+    public static void main(String[] args) {
+        int n = 1;
+        for (int i=1;i<=8;i++){
+            //statement
+
+            for (int j=1;j<=i;j++){
+                //statement
+                if (i==8||i==j||j==1) {
+                    System.out.print("*\t");
+                }else System.out.print(n+"\t");
+                n++;
+            } //j
+            System.out.println();
+        } //i
+
+
+    } //main
+
+
+} //class

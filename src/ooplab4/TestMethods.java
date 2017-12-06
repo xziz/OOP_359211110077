0f43000f92d1f4c74da5d667b1bfe3516a4b7162
xziz <aziz_0822@hotmail.com>
2017-12-06 11:17:08 +0700

    Test Methods

---
+++ src/ooplab4/TestMethods.java
@@ -0,0 +1,31 @@
+package ooplab4;
+
+public class TestMethods {
+//    method types
+//    type 1
+public static void A() {
+    System.out.println("Hello A");
+}//A
+//    type 2
+public static void B(int x, int y) {
+    System.out.println(x+" "+y);
+}//B
+//    type 3
+public static int C(int x, int y) {
+    return x + y;
+}//C
+
+    public static void main(String[] args) {
+    System.out.println("Hello");
+        A();
+        B(5, 10);
+        int x = 10, y = 20;
+        B(x, y);
+//        type 3 return value management
+//        1
+        int num = C(x, y);
+        System.out.print(num);
+
+    } //main
+
+} //class

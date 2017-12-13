c69c0143c54449a2b92637ab0c838849e5463391
xziz <aziz_0822@hotmail.com>
2017-12-13 10:18:54 +0700

    Oparator In Array

---
+++ src/ooplab5/OperatorInArray.java
@@ -0,0 +1,21 @@
+package ooplab5;
+
+public class OperatorInArray {
+    public static void main(String[] args) {
+        int number[] = {1, 2, 3, 4, 5};
+        String msg[] = {"Cat", "Dog", "Bird"};
+        System.out.println(msg.length);
+        int number2[] = {1, 2, 3};
+        number2 = number;
+        for (int i=0;i<number2.length;i++)
+            System.out.print(number2[i]+" ");
+
+        int number3[] = {1, 2, 3, 4, 5};
+        if (number==number2)
+            System.out.println("Yas");
+        else System.out.println("No");
+        number[4] = 500;
+        System.out.println(number2[4]);
+
+    } //main
+} //class

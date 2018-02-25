b06c0ff7593ac2ddf3410ee6bc06120d1a2f42c1
sofone <sofone.dekdee@gmail.com>
2018-02-26 03:25:27 +0700

    employee app

---
+++ src/ooplab8/EmployeeApp.java
@@ -0,0 +1,8 @@
+package ooplab8;
+public class EmployeeApp {
+    public static void main(String[] args) {
+        Employee employee = new Employee("Boy Saiyai", "Male", "20/12/1990", 20,
+                "Management", "20000");
+        System.out.println(employee.toString());
+    }//main
+}//class
